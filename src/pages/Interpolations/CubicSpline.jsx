f5c512e4e3049ec68c54349555593422da1de32f
Guerrita <53787821+Guerrita@users.noreply.github.com>
2022-11-14 15:04:36 -0500

    Interpolation pages added

---
+++ src/pages/Interpolations/CubicSpline.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { ChangeEvent, useState } from "react";
+
+const CubicSpline = () => {
+
+  const [value, setValue] = useState("");
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  return (
+  <section className="container">
+  <h2>Cubic Spline</h2>
+  <form>
+
+    <div className="md-textbox">
+      <input
+        onChange={handleChange}
+        className={`${value ? "has-value" : ""}`}
+        id="function"
+        type="text"
+      />
+      <label htmlFor="textbox">X Values</label>
+    </div>
+
+    <div className="md-textbox">
+      <input
+        onChange={handleChange}
+        className={`${value ? "has-value" : ""}`}
+        id="function"
+        type="text"
+      />
+      <label htmlFor="textbox">Y Values</label>
+    </div>
+    
+    <input type="submit" value="Calculate" className="primary-button login-button" />
+    </form>
+  </section>
+
+  );
+};
+
+
+export default CubicSpline;
