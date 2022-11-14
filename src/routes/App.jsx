f438e3a07db1171fa45fd3589fc4854a3a8148ad
acarmonag <acarmonag@eafit.edu.co>
2022-11-14 15:09:36 -0500

    routesofMatrices

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -9,6 +9,18 @@ import FixedPoint from '../pages/functions/FixedPoint';
 import MultipleRoots from '../pages/functions/MultipleRoots';
 import Newton from '../pages/functions/Newton';
 import Secant from '../pages/functions/Secant';
+import Cholesky from '../pages/Matrices/Cholesky';
+import Crout from '../pages/Matrices/Crout';
+import Doolittle from '../pages/Matrices/Doolittle';
+import Gauss_Seidel from '../pages/Matrices/GaussSeidel';
+import Jacobi from '../pages/Matrices/Jacobi';
+import SOR from '../pages/Matrices/SOR';
+import LUWithPartialPivoting from '../pages/Matrices/LUWithPartialPivoting';
+import LUwithGaussianSimpleElimination from '../pages/Matrices/LUwithGaussianSimpleElimination';
+import GaussianEliminationWithTotalPivoting from '../pages/Matrices/GaussianEliminationWithTotalPivoting';
+import GaussianSimpleElimination from '../pages/Matrices/GaussianSimpleElimination';
+import GaussianEliminationWhitPartialPivoting from '../pages/Matrices/GaussianEliminationWithPartialPivoting';
+
 
 const App = () => {
   return (
@@ -22,6 +34,17 @@ const App = () => {
           <Route exact path="/multiple-roots" component={MultipleRoots} />
           <Route exact path="/newton" component={Newton} />
           <Route exact path="/secant" component={Secant} />
+          <Route exact path="/cholesky" component={Cholesky} />
+          <Route exact path="/crout" component={Crout} />
+          <Route exact path="/doolittle" component={Doolittle} />
+          <Route exact path="/gauss-seidel" component={Gauss_Seidel} />
+          <Route exact path="/jacobi" component={Jacobi} />
+          <Route exact path="/sor" component={SOR} />
+          <Route exact path="/lu-with-partial-pivoting" component={LUWithPartialPivoting} />
+          <Route exact path="/lu-with-gaussian-simple-elimination" component={LUwithGaussianSimpleElimination} />
+          <Route exact path="/gaussian-elimination-with-total-pivoting" component={GaussianEliminationWithTotalPivoting} />
+          <Route exact path="/gaussian-simple-elimination" component={GaussianSimpleElimination} />
+          <Route exact path="/gaussian-elimination-with-partial-pivoting" component={GaussianEliminationWhitPartialPivoting} />
         </Switch>
       </Layout>
     </BrowserRouter>
